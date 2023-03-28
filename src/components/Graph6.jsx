0a5918617548a412067a5a420a26ba321954090f
seungwoo-woo <seungwoo.woo@gmail.com>
2023-03-28 19:35:25 +0900

    월 수출금액 표기

--- src/components/Graph6.jsx
+++ src/components/Graph6.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import XMLParser from "react-xml-parser";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -24,19 +25,22 @@ const Graph6 = () => {
 
 
   useEffect(() => {
+    
     const getData = async () => {
       const response = await axios.get(
         // "https://kosis.kr/openapi/indIdDetailSearchRequest.do?method=getList&apiKey=ZjU4Y2EwZDM4YzdhZWYxM2ZiYTk0ZTg0OTFmNTJlYmI=&format=json&jipyoId=380&strtPrdDe=201801&endPrdDe=202312&service=4&numOfRows=100&pageNo=1&serviceDetail=indIdDetail&jsonVD=Y");
         // "https://ecos.bok.or.kr/api/StatisticSearch/UWANNVLD949PL12CQV0D/JSON/kr/1/100/902Y016/A/1980/2023/KOR");
         "https://apis.data.go.kr/1220000/Newtrade/getNewtradeList?serviceKey=zwqG4%2FTWi%2FB15yaYJgvc10TbN1y81Bd86BBa6Gn2rrr6FLkl%2FGSxWO75VTMrm4KjxBKGrVqhLOTWdue6yzY%2BuQ%3D%3D&strtYymm=201501&endYymm=201512");
         console.log(response.data);
-      // setData(response.data);
 
-      setData(response.data.StatisticSearch.row);
+        const dataArr = new XMLParser().parseFromString(response.data).children;
+        console.log(dataArr[1].children[0].children.pop());
+
+      setData(dataArr[1].children[0].children);
 
       const response1 = await axios.get(
         // "http://ecos.bok.or.kr/api/StatisticSearch/UWANNVLD949PL12CQV0D/JSON/kr/1/1000/901Y014/M/201801/202312/1070000");
-        "https://ecos.bok.or.kr/api/StatisticSearch/UWANNVLD949PL12CQV0D/JSON/kr/1/100/902Y002/A/1980/2023/3010101");
+        "https://ecos.bok.or.kr/api/StatisticSearch/UWANNVLD949PL12CQV0D/JSON/kr/1/100/902Y002/M/201501/201512/3010101");
       
       console.log(response1.data);
       
@@ -49,12 +53,12 @@ const Graph6 = () => {
 
   const processedData = {
     // labels: data.map((item) => item.prdDe),
-    labels: data.map((item) => item.TIME),
+    labels: data.map((item) => item.children[5].value),
     datasets: [
       {
         label: "일평균 수출금액",
         // data: data.map((item) => Number(item.val)),
-        data: data.map((item) => Number(item.DATA_VALUE)),
+        data: data.map((item) => Number(item.children[2].value)),
         fill: true,
         cubicInterpolationMode: 'monotone',
         tension: 0.4,
